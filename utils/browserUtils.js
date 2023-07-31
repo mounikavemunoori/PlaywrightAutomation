a750cc2370322ae11ff689255c2479a93fbe016c
mounikavemunoori <mounikavemunoori@gmail.com>
2023-07-31 10:13:01 +0530

    Added the comments for browser utils

--- utils/browserUtils.js
+++ utils/browserUtils.js
@@ -5,37 +5,68 @@ export class BrowserUtils extends Utils {
         super()
         this.page = page
     }
-
+    /**
+    * @Desc Clicks on element
+    * @param {*} element - String
+    * @param {*} timeout - Waits for some time to before do the browser action
+    */
     async clickOnElement(element, timeOut) {
         timeOut = timeOut != undefined ? timeOut : 0
         await this.page.waitForTimeout(timeOut)
         await this.page.click(element)
     }
 
+    /**
+     * @Desc Inputs the value to the elements
+     * @param element - String
+     * @param value - value 
+     * @param timeout - Waits for some time to before do the browser action
+     * @return None
+     */
     async setInputField(element, value, timeOut) {
         timeOut = timeOut != undefined ? timeOut : 0
         await this.page.waitForTimeout(timeOut)
         await this.page.fill(element, value);
     }
 
-    // Type the text character by character
+    /**
+     * @Desc Types the text character by character
+     * @param element - String
+     * @param textToType - value 
+     * @return None
+     */
     async setInputFieldCharByChar(element, textToType) {
         await this.page.locator(element).type(textToType)
     }
 
+    /**
+    * @Desc waits until element is disappeared
+    * @param element - String
+    * @param timeout - Waits for some time to before do the browser action 
+    * @return None
+    */
     async waitUntilElementDisAppeared(element, timeOut) {
         timeOut = timeOut != undefined ? timeOut : 0
         await this.page.waitForTimeout(timeOut)
         await this.page.waitForSelector(element, { state: 'hidden' });
     }
 
-    // Get the text content of the last element
+    /**
+     * @Desc Gets the element inner text
+     * @param element
+     * @returns {string}
+     */
     async getElementTextContent(element, timeOut) {
         timeOut = timeOut != undefined ? timeOut : 0
         await this.page.waitForTimeout(timeOut)
         return await element.innerText();
     }
 
+    /**
+     * @Desc Gets the text of the elements
+     * @param selector
+     * @returns {*}
+     */
     async getAllElementText(selector, timeOut) {
         timeOut = timeOut != undefined ? timeOut : 0
         await this.page.waitForTimeout(timeOut)
@@ -45,43 +76,80 @@ export class BrowserUtils extends Utils {
         return elementTexts;
     }
 
-    // Wait until the page's DOM content is loaded
+    /**
+     * @Desc Wait until the page's DOM content is loaded
+     * @returns None
+     */
     async waitUntilDOMLoaded() {
         await this.page.waitForLoadState('domcontentloaded');
     }
 
+    /**
+     * @Desc waits until element is displayed
+     * @param element - String
+     * @param timeout - Waits for some time to before do the browser action 
+     * @return None
+     */
     async waitUntilElementDisplayed(element, timeOut) {
         timeOut = timeOut != undefined ? timeOut : 0
         await this.page.waitForTimeout(timeOut)
         await this.page.waitForSelector(element);
     }
 
+    /**
+     * @Desc scrolls  into the element
+     * @param element
+     * @return None
+     */
     async scrollIntoViewElement(element) {
         await element.scrollIntoViewIfNeeded();
     }
 
+    /**
+     * @Desc Navigato to the main application URL
+     * @param None
+     * @returns None
+     */
     async navigateToBrowser() {
         await this.page.context().clearCookies()
         await this.page.goto('', { viewport: null });
         await this.waitUntilDOMLoaded()
-
     }
 
+    /**
+     * @Desc Closing the opened browser
+     * @param None
+     * @returns None
+     */
     async browserClose() {
         await this.page.close()
     }
 
+    /**
+     * @Desc Taken screenshot for failed test cases based on test case name
+     * @param element
+     * @returns None
+     */
     async takeScreenShot(testInfo) {
         const reprotsPath = await this.createReportDirectory()
         await this.page.screenshot({ path: `${reprotsPath}/${testInfo.title}.png` });
         console.log("Taken screenshot for failed test cases")
     }
 
-    //  Check if the element is visible
+    /**
+     * @Desc Check if the element is visible
+     * @param element
+     * @returns {boolean}
+     */
     async isElementVisible(element) {
         return await this.page.isVisible(element)
     }
 
+    /**
+    * @Desc Gets the element text content
+    * @param element
+    * @returns {string}
+    */
     async getElementContent(element) {
         return await this.page.locator(element).textContent()
     }
